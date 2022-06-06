d01fc88c1b135e812e646a6be26c0ad633e978e7
GatoMamado <lu.morgado@duocuc.cl>
2022-06-06 12:52:19 -0400

    3.6
    -Se rellenaron las Tablas de BD

---
+++ TablasBD/olympusDropTable.sql
@@ -0,0 +1,16 @@
+drop table "OLYMPUS"."AUTH_GROUP" cascade constraints PURGE;
+drop table "OLYMPUS"."AUTH_GROUP_PERMISSIONS" cascade constraints PURGE;
+drop table "OLYMPUS"."AUTH_PERMISSION" cascade constraints PURGE;
+drop table "OLYMPUS"."CORE_CATEGORIA" cascade constraints PURGE;
+drop table "OLYMPUS"."CORE_ESTATUS" cascade constraints PURGE;
+drop table "OLYMPUS"."CORE_PRODUCTO" cascade constraints PURGE;
+drop table "OLYMPUS"."DJANGO_ADMIN_LOG" cascade constraints PURGE;
+drop table "OLYMPUS"."DJANGO_CONTENT_TYPE" cascade constraints PURGE;
+drop table "OLYMPUS"."DJANGO_MIGRATIONS" cascade constraints PURGE;
+drop table "OLYMPUS"."DJANGO_SESSION" cascade constraints PURGE;
+drop table "OLYMPUS"."USERS_COMUNA" cascade constraints PURGE;
+drop table "OLYMPUS"."USERS_DIRECCION" cascade constraints PURGE;
+drop table "OLYMPUS"."USERS_REGION" cascade constraints PURGE;
+drop table "OLYMPUS"."USERS_USERPROFILE" cascade constraints PURGE;
+drop table "OLYMPUS"."USERS_USERPROFILE_GROUPS" cascade constraints PURGE;
+drop table "OLYMPUS"."USERS_USERPROFILE_USER_PER709B" cascade constraints PURGE;
