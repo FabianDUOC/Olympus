03dddf22f27ae4428d65184ee8ec82c8cf23e6a9
FabianDUOC <fa.marileo@duocuc.cl>
2022-06-22 00:46:25 -0400

    4.2.3
    Agregadas urls de las api

--- api_rest/urls.py
+++ api_rest/urls.py
@@ -1,17 +1,26 @@
 from django.urls import path
 from api_rest.views import (listaProductos, agregarProducto, controlProducto,
-                            listaCategorias, agregarCategoria, 
-                            listaRegiones, 
-                            listaComunas)
+                            listaCategorias, agregarCategoria, controlCategoria, 
+                            listaRegiones, agregarRegion, controlRegion,
+                            listaComunas, agregarComuna, controlComuna)
 from api_rest.viewsLogin import login
 
 urlpatterns = [
     path('listaProductos/',listaProductos,name="listaProductos"),
     path('agregarProducto/',agregarProducto,name="agregarProducto"),
     path('controlProducto/<idP>/',controlProducto,name="controlProducto"),
+
     path('listaCategorias/',listaCategorias,name="listaCategorias"),
     path('agregarCategoria/',agregarCategoria,name="agregarCategoria"),
+    path('controlCategoria/',controlCategoria,name="controlCategoria"),
+
     path('listaRegiones/',listaRegiones,name="listaRegiones"),
+    path('agregarRegion/',agregarRegion,name="agregarRegion"),
+    path('controlRegion/',controlRegion,name="controlRegion"),
+
     path('listaComunas/',listaComunas,name="listaComunas"),
+    path('agregarComuna/',agregarComuna,name="agregarComuna"),
+    path('controlComuna/',controlComuna,name="controlComuna"),
+
     path('login/',login,name="login"),
 ]
